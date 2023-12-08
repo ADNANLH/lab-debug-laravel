80cd8581cdc441be8ead2c885f3456e0790c1d66
ADNANLH <adnanharrak5@gmail.com>
2023-12-08 15:32:22 +0100

    lab-debug-laravel 08-12

---
+++ calcule/app/Http/Controllers/CalculeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalculeController extends Controller
+{
+    //
+    public function somme()
+    {
+        $a = 5;
+        $b = 10;
+
+        $result = $this->add($a, $b);
+
+        return "The sum of $a and $b is: $result";
+    }
+
+    private function add($a, $b)
+    {
+        $sum = $a + $b;
+
+        return $sum;
+    }
+}
